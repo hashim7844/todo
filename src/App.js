e4c705a4da39995e91223c2a15201cb3755c3e0b
hashim7844 <159123414+hashim7844@users.noreply.github.com>
2024-02-25 06:00:03 -0700

    ss
    ss

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import './App.css';
+import Header from './component/Header';
+import React, { useEffect, useState } from 'react';
+import Footer from './component/Footer';
+import { Todos } from './component/Todos';
+import AddTodo from './component/AddTodo';
+function App() {
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  }
+  else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+
+
+  const onDelete = (todo) => {
+    console.log("I am ondelete of todo", todo);
+
+    // whh
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }));
+    console.log("deleted", todos)
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+
+  const addTodo = (title, desc) => {
+    console.log("I am adding this todo", title, desc)
+    let sno;
+    if (todos.length === 0) {
+      sno = 0;
+    }
+    else {
+      sno = todos[todos.length - 1].sno + 1;
+    }
+
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc
+    }
+    //whh
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+  }
+
+  const [todos, setTodos] = useState(initTodo);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
+
+  return (
+    <div className="app " >
+
+      <Header title="My Todos List" searchBar={true} />
+      <AddTodo addTodo={addTodo} />
+      <Todos todos={todos} onDelete={onDelete} />
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./HF.css"
+const footer = () => {
+
+  return (
+    <div className=' mt-auto text-light py-3 mt-auto HDD anch text-center '  >
+      <a className="text-center">
+        Copyright &copy; HaShim Kk </a>
+    </div>
+  )
+}
+
+export default footer
